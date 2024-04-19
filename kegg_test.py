337834180cb108ef75237f401a4c15446f41eaa9
Timothee38140 <timothee38140@gmail.com>
2024-04-19 15:27:48 +0200

    deletion fichiers inutiles

--- kegg_test.py
+++
@@ -1,9 +0,0 @@
-import requests as r
-
-cID='R00762'
-
-baseUrl="https://rest.kegg.jp/get/"
-currentUrl=baseUrl+cID
-response = r.get(currentUrl)
-truc = response.content.decode("utf8")
-print(truc)
